949d080615dcad97c10db360b8576fb958330f19
Halston <sellenth@oregonstate.edu>
2021-10-21 13:32:49 -0700

    cross-env needs to be regular dependency. updated file structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const app = require('./app')
+const {connectToDB} = require('./db')
+const port = process.env.port || 3001;
+
+app.listen(port, () => {
+    console.log("Listening on port: ", port);
+
+    if (!connectToDB()){
+        return;
+    }
+
+})
